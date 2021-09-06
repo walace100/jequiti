65311c3980bf7ecb7f2e07dd4e5ad838de11a43d
Walace Paz <walacepaz11@gmail.com>
2021-09-05 21:47:15 -0300

    criado arquivo de dados

---
+++ base.py
@@ -0,0 +1,87 @@
+base_de_dados = {
+    'merda': [
+        'bosta',
+        'pinto',
+        'merda',
+        'mijo'
+    ],
+    'cozinha': [
+        'colher',
+        'panela',
+        'geladeira'
+    ],
+    'comida': [
+        'pão',
+        'arroz',
+        'carne',
+        'feijão',
+        'farofa pronta'
+    ],
+    'carro': [
+        'roda',
+        'volante',
+        'carburador',
+        'motor',
+        'suspensão'
+    ],
+    'escritório': [
+        'caneta',
+        'impressora',
+        'papel',
+        'computador',
+        'grampeador'
+    ],
+    'computador': [
+        'mouse',
+        'teclado',
+        'monitor',
+        'headset',
+        'gabinete'
+    ],
+    'quarto': [
+        'cama',
+        'ventilador',
+        'travesseiro'
+    ],
+    'cores': [
+        'azul',
+        'preto',
+        'vermelho',
+        'verde',
+        'amarelo',
+        'branco',
+        'rosa',
+        'roxo',
+        'laranja',
+        'cinza'
+    ],
+    'estados': [
+        'Tocantins',
+        'Rondônia',
+        'Roraima',
+        'Amapá',
+        'Alagoas',
+        'Rio grande do Sul',
+        'Rio de Janeiro',
+        'São Paulo',
+        'Destrito Federal',
+        'Goiás',
+        'Mato Grosso',
+        'Ceará',
+        'Espirito Santo',
+        'Mato Grosso do Sul',
+        'Amazonas',
+        'Pará',
+        'Acre'
+    ],
+    'países': [
+        'Brasil',
+        'Estados Unidos'
+        'Canadá',
+        'México',
+        'Colombia',
+        'Argentina',
+        'Suriname',
+        'Portugal'
+    ]
+}
