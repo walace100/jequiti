075c2449f79972a15a78d001e1fa40acd16cf433
Walace Paz <walacepaz11@gmail.com>
2021-09-07 22:29:45 -0300

    trocar letras feito

--- jequiti.py
+++ jequiti.py
@@ -1,5 +1,6 @@
 from random import randint
-from re import sub
+from re import sub, search, finditer
+import unicodedata
 from base import base_de_dados
 
 jogadores = {'Ana': 0, 'Bárbara': 0, 'Carlos': 0}
@@ -7,16 +8,128 @@ jogador_ativo = 'Ana'
 valor_roleta = 0
 tema = ''
 palavras = []
-letras_erradas = []
-rodada = 0
-turno = 0
+palavras_mascaradas = []
+letras_usadas = []
+valor_rodada = 0
+valor_turno = 0
 
-def main():
+
+def pre_turno():
+    limpar_vars()
     definir_tema()
     definir_palavras()
-    passa_vez()
-    painel()
-    roleta()
+    definir_mascaras()
+
+def rodada():
+    for i in range(3):
+        pre_turno()
+        definir_rodada(i + 1)
+        turno()
+        adicionar_turno(True)
+
+
+def turno():
+    while True:
+        adicionar_turno()
+        rodar_roleta()
+        painel()
+
+        if not isinstance(valor_roleta, int):
+            valor_roleta()
+            continue
+
+        acertou_pergunta = pergunta()
+
+        if not acertou_pergunta:
+            passa_vez()
+
+        if falta_letras():
+            painel()
+            break
+
+
+def falta_letras():
+    falta_total = 0
+    for p in palavras_mascaradas:
+        falta_total += str.count(p, '_')
+
+    if falta_total <= 3:
+        return True
+    return False
+
+def pergunta():
+    while True:
+        resposta = input('Valendo {} reais, uma letra: '.format(valor_roleta))
+        resposta_validada = validar_resposta(resposta)
+
+        if not resposta_validada[0]:
+            print(resposta_validada[1])
+            continue
+
+        adicionar_letra_usada(resposta)
+        return substituir_letras_mascarada(resposta)
+
+
+def substituir_letras_mascarada(letra):
+    palavra = sem_acento(letra)
+    acertou_letra = False
+    palavras_sem_acento = []
+
+    for p in palavras:
+        list.append(palavras_sem_acento, sem_acento(p))
+
+    for i in range(len(palavras_sem_acento)):
+        pos = []
+        if palavra in palavras_sem_acento[i]:
+            acertou_letra = True
+            for occ in finditer(palavra, palavras_sem_acento[i]):
+                list.append(pos, occ.start())
+
+        for p in pos:
+            palavras_mascaradas[i] = palavras_mascaradas[i][:p] + palavra + palavras_mascaradas[i][p + 1:]
+
+    return acertou_letra
+
+def sem_acento(palavra):
+    return unicodedata.normalize('NFKD', palavra).encode('ASCII', 'ignore').decode("utf-8") 
+
+def validar_resposta(resposta):
+    if search('[\u00C0-\u017F]', resposta):
+        return (False, 'Digite uma letra sem acento. Tente novamente.')
+    elif len(resposta) > 1:
+        return (False, 'Digite apenas uma letra. Tente novamente.')
+    elif resposta in letras_usadas:
+        return (False, 'A letra já foi utilizada. Tente novamente.')
+    else:
+        return (True,)
+
+
+def definir_rodada(r):
+    global valor_rodada
+    valor_rodada = r
+
+
+def limpar_vars():
+    global palavras
+    global palavras_mascaradas
+    global letras_usadas
+
+    palavras_mascaradas = []
+    palavras = []
+    letras_usadas = []
+
+def adicionar_letra_usada(letra):
+    list.append(letras_usadas, letra)
+
+
+def adicionar_turno(zerar = False):
+    global valor_turno
+    
+    if zerar:
+        valor_turno = 0
+    else:
+        valor_turno += 1
+
 
 def definir_tema(aleatorio = True, i = 0):
     global tema
@@ -42,15 +155,29 @@ def definir_palavras():
         palavras = base_de_dados[tema]
 
 
+def definir_mascaras():
+    for p in palavras:
+        list.append(palavras_mascaradas, mascara(p))
+
+
 def roleta():
     valores = []
     funcoes = [passa_vez, perdeu_tudo]
     for i in range(100, 951, 50):
         list.insert(valores, randint(0, len(valores)), i)
     
-    for i in range(0, 2):
+    for i in range(2):
         for func in funcoes:
             list.insert(valores, randint(0, len(valores)), func)
+
+    return valores
+
+
+def rodar_roleta():
+    global valor_roleta
+
+    roleta_rodada = roleta()
+    valor_roleta = roleta_rodada[randint(0, len(roleta_rodada) -1)]
 
 
 def passa_vez():
@@ -60,16 +187,17 @@ def passa_vez():
     if posicao < len(jogadores):
         jogador_ativo = pegar_nome_por_numero(jogadores, posicao)
     else:
-        jogador_ativo = jogadores[pegar_nome_por_numero(jogadores, 0)]
+        jogador_ativo = pegar_nome_por_numero(jogadores, 0)
 
 
 def perdeu_tudo():
     jogadores[jogador_ativo] = 0
+    passa_vez()
 
 
 def painel():
     print('+', '=' * 48, sep='')
-    print('RODADA {} - TURNO {}'.format(rodada, turno))
+    print('RODADA {} - TURNO {}'.format(valor_rodada, valor_turno))
     print('+', '=' * 48, sep='')
 
     for i in range(len(jogadores.keys())):
@@ -84,10 +212,10 @@ def painel():
     print('+', '=' * 48, sep='')
     print('Tema: {}'.format(tema))
     print('+', '=' * 48, sep='')
-    print('P1) {}'.format(mascara(palavras[0])))
-    print('P2) {}'.format(mascara(palavras[1])))
-    print('P3) {}'.format(mascara(palavras[2])))
-    print('Letras erradas: {}'.format('Nenhuma' if len(letras_erradas) == 0 else str.join(letras_erradas, ', ')))
+    print('P1) {}'.format(palavras_mascaradas[0]))
+    print('P2) {}'.format(palavras_mascaradas[1]))
+    print('P3) {}'.format(palavras_mascaradas[2]))
+    print('Letras já faladas: {}'.format('Nenhuma' if len(letras_usadas) == 0 else str.join(', ', letras_usadas)))
     print('+', '=' * 48, sep='')
 
 
@@ -103,4 +231,4 @@ def mascara(palavra):
     return sub('\w', '_', palavra)
 
 
-main()
+rodada()
